0ebc521e96069003340c1da4475c907078f07bd5
neojarvis <techadmin@iamneo.ai>
2024-01-11 07:34:45 +0000

    Auto-save-commit-1/11/2024--1:04:46-PM

--- employeeadmin/routers/employeeRouter.js
+++ employeeadmin/routers/employeeRouter.js
@@ -5,22 +5,21 @@ const {validateToken} = require('../authUtil');
 const router = express.Router();
 
   // Get all Employee Details
-  router.get('/employee',validateToken, employeeController.getAllEmployees);
+  router.get('/employee', employeeController.getAllEmployees);
   
   // Get a particular Employee Details
-  router.get('/employee/user/:userid',validateToken, employeeController.getEmployeeById);
+  router.get('/employee/user/:userid', employeeController.getEmployeeById);
   
   // Employee Registration
-  router.post('/employee',validateToken, employeeController.registerEmployee);
+  router.post('/employee', employeeController.registerEmployee);
   
   // Edit Employee Details
-  router.put('/employee/:employeeid',validateToken, employeeController.editEmployee);
+  router.put('/employee/:employeeid', employeeController.editEmployee);
   
   // Delete Employee Details
-  router.delete('/employee/:employeeid',validateToken, employeeController.deleteEmployee);
+  router.delete('/employee/:employeeid', employeeController.deleteEmployee);
 
-  router.get('/users',validateToken,userController.getAllUsers);
+  router.get('/users', userController.getAllUsers);
 
   
 module.exports = router;
-  
